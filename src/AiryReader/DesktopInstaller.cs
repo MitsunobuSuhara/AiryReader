9b2c190413c135428b87d7e83bbb3dfa2dd7a940
Mitsunobu Suhara <mitsunobu.suhara@gmail.com>
2026-09-16 16:23:30 +0900

    バージョン1.4.4の配布準備

--- src/AiryReader/DesktopInstaller.cs
+++ src/AiryReader/DesktopInstaller.cs
@@ -134,7 +134,7 @@ public static class DesktopInstaller
         Microsoft.Win32.Registry.CurrentUser.DeleteSubKeyTree(@"Software\Microsoft\Windows\CurrentVersion\Uninstall\airyPDF", false);
         using var uninstall = Microsoft.Win32.Registry.CurrentUser.CreateSubKey(@"Software\Microsoft\Windows\CurrentVersion\Uninstall\AiryReader");
         uninstall.SetValue("DisplayName", "AiryReader");
-        uninstall.SetValue("DisplayVersion", "1.4.3");
+        uninstall.SetValue("DisplayVersion", "1.4.4");
         uninstall.SetValue("Publisher", "AiryReader");
         uninstall.SetValue("InstallLocation", folder);
         uninstall.SetValue("DisplayIcon", IconLocation(exe));
